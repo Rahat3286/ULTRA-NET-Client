52a66046c38cde5fd079f7cf3382627e550941f5
Rahat <rahat3286@gmail.com>
2021-04-16 23:27:01 +0600

    make some changes

--- src/comonents/User/Book/Book.js
+++ src/comonents/User/Book/Book.js
@@ -3,17 +3,19 @@ import Sidebar from '../../Dashboard/Sidebar/Sidebar';
 import { useForm } from 'react-hook-form';
 import { UserContext } from '../../../App';
 import './Book.css';
+import { useElements, useStripe } from '@stripe/react-stripe-js';
 
 const Book = () => {
-    const [loggedInUser, setLoggedInUser] =useContext(UserContext);
+
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = (data) =>{
-        console.log(data);
+    const onSubmit = (event) => {
+        console.log(event);
     }
     return (
         <div className="row">
             <div className="col-md-2">
-                <Sidebar/>
+                <Sidebar />
             </div>
             <div className="col-md-10">
                 <div className="container py-5">
@@ -27,7 +29,6 @@ const Book = () => {
                         {errors.email && <span className="error">Name is required</span>}
                         <input name="service"  {...register("service", { required: true })} placeholder="Enter the service you want" />
                         {errors.service && <span className="error">Which Service do you want?</span>}
-                        <br/>
                         <button className="btn btn-brand text-white mt-4">Pay</button>
                     </form>
                 </div>
@@ -37,3 +38,69 @@ const Book = () => {
 };
 
 export default Book;
+
+
+
+
+
+
+// maybe will workerimport React, { useContext } from 'react';
+// import Sidebar from '../../Dashboard/Sidebar/Sidebar';
+// import { useForm } from 'react-hook-form';
+// import { UserContext } from '../../../App';
+// import './Book.css';
+// import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+
+// const Book = () => {
+//     const stripe = useStripe();
+//     const elements = useElements();
+
+//     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+//     const { register, handleSubmit, watch, formState: { errors } } = useForm();
+//     const onSubmit = (event) => {
+//         event.preventDefault();
+//         console.log(event);
+
+//         if (!stripe || !elements) {
+//             return;
+//         }
+
+//         const cardElement = elements.getElement(CardElement);
+
+//         const { error, paymentMethod } = stripe.createPaymentMethod({
+//             type: 'card',
+//             card: cardElement,
+//         });
+
+//         if (error) {
+//             console.log('[error]', error);
+//         } else {
+//             console.log('[PaymentMethod]', paymentMethod);
+//         }
+//     }
+//     return (
+//         <div className="row">
+//             <div className="col-md-2">
+//                 <Sidebar />
+//             </div>
+//             <div className="col-md-10">
+//                 <div className="container py-5">
+//                     <h1 className="text-brand">Book</h1>
+//                 </div>
+//                 <div>
+//                     <form className="book-form" onSubmit={handleSubmit(onSubmit)}>
+//                         <input name="name" defaultValue={loggedInUser.name} {...register("name", { required: true })} placeholder="Your Name" />
+//                         {errors.name && <span className="error">Name is required</span>}
+//                         <input name="email" defaultValue={loggedInUser.email} {...register("email", { required: true })} placeholder="Your Name" />
+//                         {errors.email && <span className="error">Name is required</span>}
+//                         <input name="service"  {...register("service", { required: true })} placeholder="Enter the service you want" />
+//                         {errors.service && <span className="error">Which Service do you want?</span>}
+//                         <button className="btn btn-brand text-white mt-4">Pay</button>
+//                     </form>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Book;
--- src/comonents/User/PaymentProcess/PaymentProcess.js
+++ src/comonents/User/PaymentProcess/PaymentProcess.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import Book from '../Book/Book';
+
+
+const stripePromise = loadStripe('pk_test_51Ie1iKIepqUdlQr1YEoDJ6R92y5h16l89uwGt2LbgukmNnC6JqVJiIjvS3mSk3bafIcZzT7SLOrVuDf2BcYDrqTK00rxaRqPIx');
 
 const PaymentProcess = () => {
     return (
-        <div>
-            
-        </div>
+        <Elements stripe={stripePromise}>
+            <Book/>
+        </Elements>
     );
 };
 
