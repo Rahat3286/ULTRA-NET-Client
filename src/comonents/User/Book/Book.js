930a2aaeeb6171d71f7cf8e4877d8efaabb97559
Rahat <rahat3286@gmail.com>
2021-04-16 14:42:55 +0600

    make some changes

--- src/comonents/User/Book/Book.js
+++ src/comonents/User/Book/Book.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Sidebar from '../../Dashboard/Sidebar/Sidebar';
+import { useForm } from 'react-hook-form';
+import { UserContext } from '../../../App';
+import './Book.css';
 
 const Book = () => {
+    const [loggedInUser, setLoggedInUser] =useContext(UserContext);
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = (data) =>{
+        console.log(data);
+    }
     return (
         <div className="row">
             <div className="col-md-2">
                 <Sidebar/>
             </div>
             <div className="col-md-10">
-                <div className="container">
-                    <h1>this is booking section</h1>
+                <div className="container py-5">
+                    <h1 className="text-brand">Book</h1>
+                </div>
+                <div>
+                    <form className="book-form" onSubmit={handleSubmit(onSubmit)}>
+                        <input name="name" defaultValue={loggedInUser.name} {...register("name", { required: true })} placeholder="Your Name" />
+                        {errors.name && <span className="error">Name is required</span>}
+                        <input name="email" defaultValue={loggedInUser.email} {...register("email", { required: true })} placeholder="Your Name" />
+                        {errors.email && <span className="error">Name is required</span>}
+                        <input name="service"  {...register("service", { required: true })} placeholder="Enter the service you want" />
+                        {errors.service && <span className="error">Which Service do you want?</span>}
+                        <br/>
+                        <button className="btn btn-brand text-white mt-4">Pay</button>
+                    </form>
                 </div>
             </div>
         </div>
