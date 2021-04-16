d64349393fffe8c013e6ad372950b23676655d8f
Rahat <rahat3286@gmail.com>
2021-04-16 17:22:10 +0600

    make some changes

--- src/comonents/Home/Testimonials/Testimonials.js
+++ src/comonents/Home/Testimonials/Testimonials.js
@@ -1,40 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TestimonialsCard from '../TestimonialsCard/TestimonialsCard';
 import customerImage1 from '../../../Images/customerImage1.jpg';
 import customerImage2 from '../../../Images/customerImage2.jpg';
 import customerImage3 from '../../../Images/customerImage3.jpg';
 
-const testimonialsData = [
-    {
-        image: customerImage1,
-        name: 'Nash Patrik',
-        ranking: 'CEO',
-        location: 'Manpol',
-        description: 'Praesent tincidunt porttitor libero, id sodales enim tincidunt. Proin dignissim leo finibus risus.'
-    },
-    {
-        image: customerImage2,
-        name: 'Miriam Barron',
-        ranking: 'CEO',
-        location: 'Manpol',
-        description: 'Praesent tincidunt porttitor libero, id sodales enim tincidunt. Proin dignissim leo finibus risus.'
-    },
-    {
-        image: customerImage3,
-        name: 'Bria Malone',
-        ranking: 'CEO',
-        location: 'Manpol',
-        description: 'Praesent tincidunt porttitor libero, id sodales enim tincidunt. Proin dignissim leo finibus risus.'
-    },
-]
-
 const Testimonials = () => {
+
+    const [reviews,setReviews] = useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/reviews')
+        .then(res=>res.json())
+        .then(data=>setReviews(data))
+    },[])
     return (
         <div className="container mt-5 pt-5">
             <h1 className="text-center" style={{ color: 'rgb(31, 23, 49)' }}>Testimonials</h1>
             <div className="row mt-5">
                 {
-                    testimonialsData.map(testimonial => <TestimonialsCard testimonial={testimonial} key={testimonial.name}></TestimonialsCard>)
+                    reviews.map(testimonial => <TestimonialsCard testimonial={testimonial} key={testimonial._id}></TestimonialsCard>)
                 }
             </div>
         </div>
--- src/comonents/User/Review/Review.js
+++ src/comonents/User/Review/Review.js
@@ -1,15 +1,77 @@
-import React from 'react';
+import React, { useContext, useState} from 'react';
 import Sidebar from '../../Dashboard/Sidebar/Sidebar';
+import { useForm  } from 'react-hook-form';
+import axios from 'axios';
+import { UserContext } from '../../../App';
+import './Review.css';
 
 const Review = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [image, setImage] = useState(null);
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    const handleImageUpload = event => {
+        console.log(event.target.files[0])
+        const imageData = new FormData();
+        imageData.set('key', '32dbb43d356182fea99f3eb509f510cb');
+        imageData.append('image', event.target.files[0])
+
+        axios.post('https://api.imgbb.com/1/upload', imageData)
+            .then(function (response) {
+                setImage(response.data.data.display_url);
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+
+    const onSubmit = (data) => {
+        const reviewsData = {
+            image: image,
+            name: data.name,
+            ranking: data.ranking,
+            location: data.location,
+            description: data.description
+        }
+        console.log(reviewsData);
+        fetch('http://localhost:5000/addReviews',{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(reviewsData)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data){
+                alert('Thanks for your feedback')
+            }
+        })
+    }
     return (
         <div className="row">
             <div className="col-md-2">
-                <Sidebar/>
+                <Sidebar />
             </div>
             <div className="col-md-10">
                 <div className="container">
                     <h1>this review section</h1>
+                </div>
+                <div>
+                    <form className="review-form" onSubmit={handleSubmit(onSubmit)}>
+                        <input name="name" defaultValue={loggedInUser.name} {...register("name", { required: true })} placeholder="Your Name" />
+                        {errors.name && <span className="error">Name is required</span>}
+                        <input name="ranking"  {...register("ranking", { required: true })} placeholder="Enter your rank " />
+                        {errors.ranking && <span className="error">Your ranking is required</span>}
+                        <input name="location"  {...register("location", { required: true })} placeholder="Enter your location " />
+                        {errors.location && <span className="error">What is your location?</span>}
+                        <input name="image" type="file" onChange={handleImageUpload} />
+                        <input name="description" {...register("description", { required: true })} placeholder="Write Your Thoughts"/>
+                        <br />
+                        <button className="btn btn-brand text-white mt-4">Submit</button>
+                    </form>
                 </div>
             </div>
         </div>
